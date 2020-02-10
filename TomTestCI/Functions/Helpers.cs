71da18d10618ef294436b13804c8a9ad4db13e30
Tom Gao <tom.gao@dinebrands.com>
2020-02-10 09:53:11 -0800

    Add project files.

---
+++ TomTestCI/Functions/Helpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TomsTestCI.Functions
+{
+    public static class Helpers
+    {
+        public static string GetLOL(string s)
+        {
+            return s + "LOL";
+        }
+    }
+}
---
+++ TomTestCI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TomTestCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
