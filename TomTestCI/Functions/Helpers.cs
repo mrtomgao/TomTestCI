05fcd7ee6623c590de4922ebef5e93f911ff75c6
Tom Gao <tom.gao@dinebrands.com>
2020-02-21 13:18:04 -0800

    TOMCI-4  removed some shizz

--- TomTestCI/Functions/Helpers.cs
+++ TomTestCI/Functions/Helpers.cs
@@ -11,9 +11,5 @@ namespace TomsTestCI.Functions
             return s + "Tom";
         }
 
-        public static string GetLeon(string s)
-        {
-            return s + "Leon";
-        }
     }
 }
